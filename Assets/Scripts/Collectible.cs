733f2dd7e78a0ada5fbd5af01523e06b8e3a249b
jadeeu <S10273501@connect.np.edu.sg>
2026-06-10 12:19:42 +0800

    collect_script

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Collectible : MonoBehaviour
+public class KeyCard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.CompareTag("Player"))
+        {
+            GameManager.Instance.AddKeyCard();
+            Destroy(gameObject);
+        }
     }
 }
