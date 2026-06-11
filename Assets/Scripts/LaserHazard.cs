21635b73e1edcf75c403df5ce016f168663c2cf0
jadeeu <S10273501@connect.np.edu.sg>
2026-06-11 12:52:52 +0800

    lazer_debugging

--- Assets/Scripts/LaserHazard.cs
+++ Assets/Scripts/LaserHazard.cs
@@ -6,8 +6,12 @@ public class LaserHazard : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("TRIGGER HIT: " + other.name);
+
         if (other.CompareTag("Player"))
         {
+            Debug.Log("PLAYER HIT LASER");
+
             other.transform.position = checkpoint.position;
         }
     }
